35a8aa5a6cd6ccff956350b0fa061c1cb8cf09f0
hani <hani_haha@naver.com>
2020-08-18 14:05:39 +0900

    aaaaa

--- app/src/main/java/kosmo/hdpay/RegisterActivity.java
+++ app/src/main/java/kosmo/hdpay/RegisterActivity.java
@@ -2,13 +2,6 @@ package kosmo.hdpay;
 import android.os.AsyncTask;
 import android.webkit.CookieManager;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.google.gson.Gson;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,8 +9,6 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class RegisterActivity extends AsyncTask<String, Void, String> {
     String sendMsg, receiveMsg;
@@ -72,79 +63,6 @@ public class RegisterActivity extends AsyncTask<String, Void, String> {
 
         //jsp로부터 받은 리턴 값
         return receiveMsg;
-    }
-
-    public void request(String urlStr){
-
-        StringRequest request = new StringRequest(
-                Request.Method.GET,
-                urlStr,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        System.out.println("응답 -> "+response);
-
-                        processResponse(response);
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.println("에러 -> "+error.toString());
-                    }
-                }
-
-        ){
-            //요청 파라미터를 처리하는 메소드. 웹에서 서버쪽으로 데이터를 보낼때
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> params = new HashMap<>();
-
-                return params;
-            }
-        };
-
-
-
-
-//        try {
-//            StringBuilder builder = new StringBuilder();
-//
-//            URL url = new URL(urlStr);
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//
-//            if(conn != null) {
-//                conn.setConnectTimeout(10000);
-//                conn.setRequestMethod("GET");
-//                conn.setDoInput(true);
-//                //conn.setDoOutput(true);
-//
-//                int resCode = conn.getResponseCode(); //요청을 보내고 받는 역할
-//
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//                String line = null;
-//                while (true) {
-//                    line = reader.readLine();
-//                    if(line == null){ //문자가 없는 경우
-//                        break;
-//                    }
-//
-//                    builder.append(line+ "/n");
-//                }
-//                reader.close();
-//                conn.disconnect();
-//            }
-//            System.out.println("응답 -> "+ builder.toString());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-
-    }
-    public void processResponse(String response){
-        Gson gson = new Gson();
-
-        MemberVo member = gson.fromJson(response, MemberVo.class);
     }
 
 
